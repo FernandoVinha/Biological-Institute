b07fa22414d3e7c347a87dd98f6315188480a23f
FernandoVinha <fernando.h.vinha@gmail.com>
2024-04-08 10:38:43 -0300

    sh modifalie

--- start.sh
+++ start.sh
@@ -1,68 +1,55 @@
 #!/bin/bash
 
+# Atualizar os pacotes e instalar o Python e o pip
+echo "Atualizando pacotes e instalando Python e pip..."
+sudo apt-get update
+sudo apt-get install -y python3 python3-venv python3-pip
+
 # Definir o caminho do diretório do projeto Django
 PROJECT_DIR=$(pwd)
-SERVICE_NAME="instituto_biologico"
+SERVICE_NAME="Biologico"
 
-# Criar e ativar o ambiente virtual
-echo "Criando e ativando o ambiente virtual..."
+# Criar ambiente virtual e instalar dependências
+echo "Criando ambiente virtual e instalando dependências..."
 python3 -m venv venv
 source venv/bin/activate
-
-
-# Instalar ngrok se não estiver instalado
-if ! command -v ngrok &> /dev/null
-then
-    echo "Instalando ngrok..."
-    curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
-    echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
-    sudo apt update
-    sudo apt install ngrok
-fi
-
-# Instalar as dependências do projeto Django dentro do ambiente virtual
-echo "Instalando as dependências do projeto Django dentro do ambiente virtual..."
 pip install -r requirements.txt
+
+# Executar migrações do Django e o comando cities_light
+echo "Executando migrações do Django e comando cities_light..."
 python manage.py makemigrations
 python manage.py migrate
-# Configurar o authtoken do ngrok
-echo "Configurando o authtoken do ngrok..."
-ngrok config add-authtoken 2emv4xRVSt3Jj3oTxkgq0BdRfDM_3SsdzXWCcf1Vj4az1qnFF
+python manage.py cities_light
 
-# Criar o script de inicialização do Instituto Biológico
-echo "Criando script de inicialização do Instituto Biológico..."
+# Criar o script de inicialização do Django
+echo "Criando script de inicialização..."
 echo "#!/bin/bash
 cd $PROJECT_DIR
-
-# Iniciar servidor Django na porta 8000
-python manage.py runserver 0.0.0.0:8000 &
-
-# Aguardar até que o Django esteja executando
-sleep 10
-
-# Iniciar ngrok
-ngrok http http://0.0.0.0:8000/" > start_instituto_biologico.sh
+source venv/bin/activate
+python manage.py runserver 177.73.234.198:8000" > start_django.sh
+#python manage.py runserver 177.73.234.198:88"
+	
 
 # Tornar o script de inicialização executável
-chmod +x start_instituto_biologico.sh
+chmod +x start_django.sh
 
-# Criar o arquivo de serviço systemd para o Instituto Biológico
+# Criar o arquivo de serviço systemd
 SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
 
 echo "[Unit]
-Description=Iniciar Servidor do Instituto Biológico e ngrok na inicialização
+Description=Iniciar Servidor Django na inicialização
 
 [Service]
-ExecStart=$PROJECT_DIR/start_instituto_biologico.sh
+ExecStart=$PROJECT_DIR/start_django.sh
 User=root
 
 [Install]
 WantedBy=multi-user.target" | sudo tee $SERVICE_FILE
 
-# Recarregar os daemons do systemd, habilitar e iniciar o serviço do Instituto Biológico
-echo "Configurando e iniciando o serviço systemd do Instituto Biológico..."
+# Recarregar os daemons do systemd, habilitar e iniciar o serviço
+echo "Configurando e iniciando o serviço systemd..."
 sudo systemctl daemon-reload
 sudo systemctl enable $SERVICE_NAME
 sudo systemctl start $SERVICE_NAME
 
-echo "Instalação do serviço do Instituto Biológico concluída!"
+echo "Instalação concluída!"
